648067da9191d8af048ff60dbf724655febd04b1
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-13 18:04:07 +0200

    Update Teacher.java

--- src/beziehungLehrerStudent/Teacher.java
+++ src/beziehungLehrerStudent/Teacher.java
@@ -1,2 +1,20 @@
-package beziehungLehrerStudent;public class Teacher {
+package beziehungLehrerStudent;
+
+import java.util.Arrays;
+
+public class Teacher extends Person{
+
+    public Teacher(String name, int personId, String branche, String[] adress) {
+        super(name, personId, branche, adress);
+    }
+
+    public void akademik(String ... values){
+        System.out.println(Arrays.toString(values));
+    }
+
+    @Override
+    public String toString() {
+        return "Hier ist es information eines Lehreres:\n"+super.toString();
+    }
 }
+
