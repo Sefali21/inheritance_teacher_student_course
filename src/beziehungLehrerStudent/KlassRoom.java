93e79a1abc85e4a3d26b94acfd4c65ed1c6c3b90
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-13 18:02:38 +0200

    Update KlassRoom.java

--- src/beziehungLehrerStudent/KlassRoom.java
+++ src/beziehungLehrerStudent/KlassRoom.java
@@ -1,2 +1,36 @@
-package beziehungLehrerStudent;public class KlassRoom {
+package beziehungLehrerStudent;
+
+public class KlassRoom extends Course{
+    private int klassRoomId;
+    private int klassRoomKapazite;
+
+    public KlassRoom(String courseName, String courseId, String courseBranche, String courseDauert, int klassRoomId, int klassRoomKapazite) {
+        super(courseName, courseId, courseBranche, courseDauert);
+        this.klassRoomId = klassRoomId;
+        this.klassRoomKapazite = klassRoomKapazite;
+    }
+
+    public int getKlassRoomId() {
+        return klassRoomId;
+    }
+
+    public void setKlassRoomId(int klassRoomId) {
+        this.klassRoomId = klassRoomId;
+    }
+
+    public int getKlassRoomKapazite() {
+        return klassRoomKapazite;
+    }
+
+    public void setKlassRoomKapazite(int klassRoomKapazite) {
+        this.klassRoomKapazite = klassRoomKapazite;
+    }
+
+    @Override
+    public String toString() {
+        return "KlassRoom{" +
+                "klassRoomId=" + klassRoomId +
+                ", klassRoomKapazite=" + klassRoomKapazite +super.toString()+
+                '}';
+    }
 }
