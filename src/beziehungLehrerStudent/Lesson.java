c3a6fcb7c11f492d888b93ed3e69d1548f222c76
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-13 18:02:58 +0200

    Update Lesson.java

--- src/beziehungLehrerStudent/Lesson.java
+++ src/beziehungLehrerStudent/Lesson.java
@@ -1,2 +1,37 @@
-package beziehungLehrerStudent;public class Lesson {
+package beziehungLehrerStudent;
+
+public class Lesson extends Course{
+    private String lessonName;
+    private String  lessonId;
+
+    public Lesson(String courseName, String courseId, String courseBranche, String courseDauert, String lessonName, String lessonId) {
+        super(courseName, courseId, courseBranche, courseDauert);
+        this.lessonName = lessonName;
+        this.lessonId = lessonId;
+    }
+
+    public String getLessonName() {
+        return lessonName;
+    }
+
+    public void setLessonName(String lessonName) {
+        this.lessonName = lessonName;
+    }
+
+    public String  getLessonId() {
+        return lessonId;
+    }
+
+    public void setLessonId(String lessonId) {
+        this.lessonId = lessonId;
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson " +super.toString() +
+                "lessonName='" + lessonName + '\'' +
+                ", lessonId=" + lessonId +
+                '}';
+    }
 }
+
