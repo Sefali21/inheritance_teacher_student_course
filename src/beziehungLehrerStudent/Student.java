2fb7bc245439420bb66efce03cd5706e15cd44bf
Sefali21 <100235906+Sefali21@users.noreply.github.com>
2023-07-13 18:03:50 +0200

    Update Student.java

--- src/beziehungLehrerStudent/Student.java
+++ src/beziehungLehrerStudent/Student.java
@@ -1,2 +1,24 @@
-package beziehungLehrerStudent;public class Student {
+package beziehungLehrerStudent;
+
+public class Student extends Person{
+
+    private double credit;
+    public Student(String name, int personId, String branche, String[] adress, double credit) {
+        super(name, personId, branche, adress);
+        this.credit=credit;
+    }
+
+    public double getCredit() {
+        return credit;
+    }
+
+    public void setCredit(double credit) {
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return "Hier ist es information eines Schuleres:\n"+super.toString()+" credit: "+credit;
+    }
 }
+
